0c75e5673f99a0af0594745a67ec7d873e421ea0
Oleksandr Pyvko <pyvko.a.g@gmail.com>
2024-10-23 13:43:16 +0300

    server-side validation and displaying error message under the form

---
+++ app/meals/share/error.js
@@ -0,0 +1,12 @@
+"use client";
+
+function error() {
+  return (
+    <main className="error">
+      <h1>Oops! Something went wrong</h1>
+      <p>Invalid input</p>
+    </main>
+  );
+}
+
+export default error;
